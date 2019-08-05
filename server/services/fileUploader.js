5f6db09d3f3a1c4ddb43bb73db7280a7ba5543f5
patrickgsmith <patrickgromersmith@gmail.com>
2019-08-05 13:41:59 -0700

    created s3 file upload service

--- server/services/fileUploader.js
+++ server/services/fileUploader.js
@@ -3,23 +3,13 @@ const express = require("express");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 
-const router = express.Router();
 const s3 = new aws.S3();
-
-aws.config.update({
-  secretAccessKey: "kF+5qB3iA9qPqGWwy75jSfZwCG2kBxBvw7MVcbrS",
-  accessKeyId: "AKIAJBZTB3Y45ZKY2YYQ",
-  region: "us-east-2"
-});
-
-console.dir(aws.config);
-process.exit(1);
 
 const upload = multer({
   storage: multerS3({
     acl: "public-read",
     s3: s3,
-    bucket: "chefsmenu",
+    bucket: "chefs-menu",
     metadata: function(req, file, cb) {
       cb(null, { fieldName: "image" });
     },
@@ -29,10 +19,5 @@ const upload = multer({
   })
 });
 
-router.post("/", upload.single("image"), function(req, res, next) {
-  console.log(req.file.location);
 
-  res.send("Successfully uploaded image file!");
-});
-
-module.exports = router;
+module.exports = upload.single("image");
