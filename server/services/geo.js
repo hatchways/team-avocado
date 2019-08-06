150b317a7932b11ef7ed674019f7a9f3ffa4b61a
patrickgsmith <patrickgromersmith@gmail.com>
2019-08-06 12:17:55 -0700

    Created geocoding service

---
+++ server/services/geo.js
@@ -0,0 +1,73 @@
+const fetch = require("node-fetch");
+const {GEO_APP_ID, GEO_APP_CODE} = process.env;
+
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  //:::                                                                              :::
+  //:::  This Function Courtesy of GeoDataSource.com (C) All Rights Reserved 2018    :::
+  //:::  Official Web site: https://www.geodatasource.com                            :::
+  //:::                                                                              :::
+  //:::                                                                              :::
+  //:::  This routine calculates the distance between two points (given the          :::
+  //:::  latitude/longitude of those points)                                         :::
+  //:::                                                                              :::
+  //:::  Definitions:                                                                :::
+  //:::    South latitudes are negative, east longitudes are positive                :::
+  //:::                                                                              :::
+  //:::  Passed to function:                                                         :::
+  //:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)       :::
+  //:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)       :::
+  //:::    unit = the unit you desire for[0] result[0]s                              :::
+  //:::           where: 'M' is statute miles (default)                              :::
+  //:::                  'K' is kilometers                                           :::
+  //:::                  'N' is nautical miles                                       :::
+  //:::                                                                              :::
+  //:::                                                                              :::
+  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  module.exports.getDistance = function (lat1, lon1, lat2, lon2, unit) {
+    if (lat1 == lat2 && lon1 == lon2) {
+      return 0;
+    } else {
+      var radlat1 = (Math.PI * lat1) / 180;
+      var radlat2 = (Math.PI * lat2) / 180;
+      var theta = lon1 - lon2;
+      var radtheta = (Math.PI * theta) / 180;
+      var dist =
+        Math.sin(radlat1) * Math.sin(radlat2) +
+        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit == "K") {
+        dist = dist * 1.609344;
+      }
+      if (unit == "N") {
+        dist = dist * 0.8684;
+      }
+      return dist;
+    }
+  }
+
+  module.exports.getCoordinates = async function (address) {
+    // Convert address string into appropriate form for URL and generate URL
+    const URL = `https://geocoder.api.here.com/6.2/geocode.json?app_id=${APP_ID}&app_code=${APP_CODE}&searchtext=${address.replace(" ", "+")}`;
+
+    // Make the request and parse returned JSON
+    const res = await fetch(URL).then(res => res.json());
+
+    console.log(res);
+
+    let result;
+    try {
+      result = {
+        lat: res.Response.View[0].Result[0].Location.MapView.TopLeft.Latitude,
+        lon: res.Response.View[0].Result[0].Location.MapView.TopLeft.Longitude
+      };
+    } catch (error) {
+      throw Error("Invalid Address");
+    }
+
+    return result;
+}
