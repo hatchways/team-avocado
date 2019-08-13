532bb3e8ef6f6c4f235963f0254fddc387d4d97c
smithpg <patrickgromersmith@gmail.com>
2019-08-13 16:46:39 -0700

    fixed typo

--- client/src/hooks/useResource.js
+++ client/src/hooks/useResource.js
@@ -1,11 +1,11 @@
 import { useState, useEffect, useContext } from "react";
-import { apiCall } from "../helpers/api";
+import { callAPI } from "../helpers/api";
 
 export default function useResource(endpoint) {
   const [resource, setResource] = useState(null);
 
   useEffect(async () => {
-    setResource(await apiCall(endpoint));
+    setResource(await callAPI(endpoint));
   }, []);
 
   return [resource];
