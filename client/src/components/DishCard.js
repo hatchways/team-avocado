6f28b97fec48e8ddda0b36bdff63a40624f83dfc
patrickgsmith <patrickgromersmith@gmail.com>
2019-08-21 12:51:13 -0700

    progress on refactor

--- client/src/components/DishCard.js
+++ client/src/components/DishCard.js
@@ -1,84 +1,89 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 import { Route, Switch } from "react-router-dom";
 import Button from "./Button";
 import styled from "styled-components";
-import TextField from '@material-ui/core/TextField';
+import TextField from "@material-ui/core/TextField";
 import { colors } from "../themes/theme";
-import Tooltip from '@material-ui/core/Tooltip';
+import Tooltip from "@material-ui/core/Tooltip";
 import { callAPI } from "../helpers/api";
 import { useContext } from "react";
 import AuthContext from "../store/createContext";
-
-
 
 const { brandLight } = colors;
 const SaveButton = styled(Button)`
   min-width: 100px;
 `;
-const useStyles = makeStyles(theme =>({
+const useStyles = makeStyles(theme => ({
   card: {
     minWidth: "70%",
-    minHeight:"43%",
-    margin:5,
+    minHeight: "43%",
+    margin: 5
   },
 
-  image:{
-    padding:40,
+  image: {
+    padding: 40
   },
 
-  form:{
-    width:"100%",
-    display: 'flex',
-    flexDirection:"row",
-    justifyContent:"space-between",
+  form: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
-  info:{
-    display: 'flex',
-    flexDirection:"column",
-    padding:40,
+  info: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 40
   },
 
-  pplnum:{
+  pplnum: {
     backgroundColor: brandLight,
-    color:"white",
-    padding:"10px",
-    display:"inline",
-    justifyContent: "space-between",
+    color: "white",
+    padding: "10px",
+    display: "inline",
+    justifyContent: "space-between"
   },
-  textField:{
-    fullWidth:"true",
+  textField: {
+    fullWidth: "true"
   },
-  input:{
-    display:"none",
+  input: {
+    display: "none"
   },
-  dishpic:{
-    cursor:"pointer",
+  dishpic: {
+    cursor: "pointer"
   },
-  name:{
+  name: {
     fontWeight: "bold",
-    fontSize: 20,
+    fontSize: 20
   },
-  price:{
-    color:colors.brandLight,
+  price: {
+    color: colors.brandLight
   },
-  bigbold:{
-    fontWeight:"bold",
+  bigbold: {
+    fontWeight: "bold"
   },
-  grey:{
-    color:"grey"
+  grey: {
+    color: "grey"
   }
-
-
 }));
 
-export default function SimpleCard({dishImg ,dish_id,name,serve,price,ingred,required,  storeUpdatedDish,storeDishImg}) {
-
+export default function SimpleCard({
+  dishImg,
+  dish_id,
+  name,
+  serve,
+  price,
+  ingred,
+  required,
+  storeUpdatedDish,
+  storeDishImg
+}) {
   const classes = useStyles(brandLight);
 
-  const {user,setUser} = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
   // console.log("Context user:",user);
   const [values, setValues] = React.useState({
     numPeopleServed: serve,
@@ -89,14 +94,12 @@ export default function SimpleCard({dishImg ,dish_id,name,serve,price,ingred,req
     cuisine: "Japanese",
     chef: user.id,
     dishImg: dishImg
-  }); 
-  
+  });
+
   const handleChange = name => event => {
     setValues({ ...values, [name]: event.target.value });
   };
-  
 
-  
   async function onSubmitAttempt(e) {
     e.preventDefault();
     try {
@@ -107,7 +110,7 @@ export default function SimpleCard({dishImg ,dish_id,name,serve,price,ingred,req
           "Content-Type": "application/json"
         },
         body: values,
-        token: user.token,
+        token: user.token
       });
 
       storeUpdatedDish(newdish);
@@ -116,113 +119,110 @@ export default function SimpleCard({dishImg ,dish_id,name,serve,price,ingred,req
     }
   }
 
-  async function handleClick(event){
+  async function handleClick(event) {
     event.preventDefault();
     const fileObj = event.target.files[0];
     let formData = new FormData();
     formData.append("image", fileObj);
     try {
-      const endpoint = `dish/${dish_id}/dishImg`
+      const endpoint = `dish/${dish_id}/dishImg`;
       const imgURL = await callAPI({
-            endpoint: endpoint,
-            method:"POST",
-            body: formData,
-            isForm: true,
-        });
-        setValues({...values, dishImg:imgURL});
-        storeDishImg(imgURL, dish_id);
+        endpoint: endpoint,
+        method: "POST",
+        body: formData,
+        isForm: true
+      });
+      setValues({ ...values, dishImg: imgURL });
+      storeDishImg(imgURL, dish_id);
     } catch (error) {
       console.log(error);
     }
-  };
+  }
   return (
     <Card className={classes.card}>
       <Switch>
+        <Route path="/chef/:chef_id/edit">
+          <form className={classes.form} onSubmit={onSubmitAttempt}>
+            <Grid className={classes.info}>
+              <TextField
+                label="How many people to serve."
+                className="{classes.textField}"
+                value={values.numPeopleServed}
+                onChange={handleChange("numPeopleServed")}
+                margin="dense"
+                variant="outlined"
+                type="number"
+              />
+              <TextField
+                label="name"
+                className="{classes.textField}"
+                value={values.name}
+                onChange={handleChange("name")}
+                margin="dense"
+                variant="outlined"
+              />
+              <TextField
+                label="Price"
+                className="{classes.textField}"
+                value={values.price}
+                onChange={handleChange("price")}
+                margin="dense"
+                variant="outlined"
+                type="number"
+              />
+              <TextField
+                label="Ingredients"
+                multiline
+                rowsMax="4"
+                className="{classes.textField}"
+                value={values.ingredients}
+                onChange={handleChange("ingredients")}
+                margin="dense"
+                variant="outlined"
+              />
 
-      <Route path="/chef/:chef_id/edit">
-      <form  className={classes.form} onSubmit={onSubmitAttempt} >
+              <TextField
+                label="Required Stuff"
+                className="{classes.textField}"
+                value={values.requirements}
+                multiline
+                rowsMax="4"
+                onChange={handleChange("requirements")}
+                margin="dense"
+                variant="outlined"
+              />
+            </Grid>
 
-        <Grid className={classes.info}>
+            <Grid className={classes.image}>
+              <input
+                accept="image/*"
+                className={classes.input}
+                id={dish_id}
+                multiple
+                type="file"
+                onChange={handleClick}
+              />
+              <label htmlFor={dish_id}>
+                <Tooltip
+                  title="Click to upload new profile"
+                  placement="top-start"
+                >
+                  <img
+                    className={classes.dishpic}
+                    alt="dish"
+                    src={values.dishImg}
+                  />
+                </Tooltip>
+              </label>
+            </Grid>
 
-          <TextField
-              label="How many people to serve."
-              className="{classes.textField}"
-              value={values.numPeopleServed}
-              onChange={handleChange('numPeopleServed')}
-              margin="dense"
-              variant="outlined"
-              type="number"
-          />
-          <TextField
-              label="name"
-              className="{classes.textField}"
-              value={values.name}
-              onChange={handleChange('name')}
-              margin="dense"
-              variant="outlined"
-          />
-          <TextField
-              label="Price"
-              className="{classes.textField}"
-              value={values.price}
-              onChange={handleChange('price')}
-              margin="dense"
-              variant="outlined"
-              type="number"
-          />
-          <TextField
-              label="Ingredients"
-              multiline
-              rowsMax="4"
-              className="{classes.textField}"
-              value={values.ingredients}
-              onChange={handleChange('ingredients')}
-              margin="dense"
-              variant="outlined"
-          />
-
-          <TextField
-              label="Required Stuff"
-              className="{classes.textField}"
-              value={values.requirements}
-              multiline
-              rowsMax="4"
-              onChange={handleChange('requirements')}
-              margin="dense"
-              variant="outlined"
-          />
-          
-        </Grid>
-
-        <Grid className={classes.image}>
-
-
-        <input
-            accept="image/*"
-            className={classes.input}
-            id={dish_id}
-            multiple
-            type="file"
-            onChange={handleClick}
-        />
-        <label htmlFor={dish_id}>
-          <Tooltip title="Click to upload new profile" placement="top-start">
-
-            <img className={classes.dishpic}  alt="dish" src={values.dishImg} />
-          </Tooltip>
-        </label>
-        </Grid>
-        
-          <SaveButton type="submit" >
-            Save Item
-          </SaveButton>
-        </form>
+            <SaveButton type="submit">Save Item</SaveButton>
+          </form>
         </Route>
 
         <Route path="/chef/:chef_id">
-        <div className={classes.form}>
-          <Grid className={classes.info}>
-
+          <div className={classes.form}>
+            <Grid className={classes.info}>
               <div>
                 <span className={classes.pplnum}> MEAL FOR {serve}</span>
               </div>
@@ -232,18 +232,15 @@ export default function SimpleCard({dishImg ,dish_id,name,serve,price,ingred,req
               <p className={classes.grey}>{ingred.join(", ")}</p>
               <span className={classes.bigbold}>REQUIRED STUFF</span>
               <p className={classes.grey}>{required.join(", ")}</p>
-              
-          </Grid>
+            </Grid>
 
-          <Grid className={classes.image}>
-              <img className="dishpic" alt="dish" src={values.dishImg}/>
-          </Grid>
+            <Grid className={classes.image}>
+              <img className="dishpic" alt="dish" src={values.dishImg} />
+            </Grid>
           </div>
         </Route>
-              {/* change to if else, if user is logged in and  */}
-              {/* this is user's profile page, show edit button */}
-              
-
+        {/* change to if else, if user is logged in and  */}
+        {/* this is user's profile page, show edit button */}
       </Switch>
     </Card>
   );
--- client/src/pages/ChefProfilePage.js
+++ client/src/pages/ChefProfilePage.js
@@ -1,60 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
-
 import Navbar from "../components/Navbar";
 import NameCard from "../components/ChefNameCard";
 import SimpleMenu from "../components/MenuButton";
 import ChefsMenu from "../components/ChefsMenu";
 import { layout } from "../themes/theme";
-
+import { useContext } from "react";
+import AuthContext from "../store/createContext";
 
 const PageContainer = styled.div`
   display: flex;
   align-items: stretch;
-  height:100vh;
+  height: 100vh;
 
   .paneLeft {
-    position:relative;
-    top:12%;
-    width: 24%; 
-    height:88%;
+    position: relative;
+    top: 12%;
+    width: 24%;
+    height: 88%;
     display: flex;
-    flex-direction:column;
+    flex-direction: column;
   }
 
   .paneRight {
-    position:relative;
-    top:12%;
-    width: 76%; 
-    height:88%;
+    position: relative;
+    top: 12%;
+    width: 76%;
+    height: 88%;
     display: flex;
-    flex-direction:column;
+    flex-direction: column;
     align-items: center;
   }
 
   nav span {
     margin-right: ${layout.spacing(4)};
-    
   }
-
 `;
 
 function ChefPage(props) {
-  const chef_id=props.match.params.chef_id;
+  const { user } = useContext(AuthContext);
+
+  const chef_id = props.match.params.chef_id,
+    userIsOwner = user.id === chef_id;
 
   return (
     <PageContainer className="pageContainer">
-
       <Navbar>
-        <SimpleMenu></SimpleMenu>
+        <SimpleMenu />
       </Navbar>
 
       <div className="paneLeft">
-        <NameCard user_id={chef_id}/>
+        <NameCard user_id={chef_id} userIsOwner={userIsOwner} />
       </div>
       <div className="paneRight">
-        <ChefsMenu user_id={chef_id}/>
+        <ChefsMenu user_id={chef_id} userIsOwner={userIsOwner} />
       </div>
     </PageContainer>
   );
