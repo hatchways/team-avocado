ec60389bbdb17d1217d92045388be57e98206b3a
patrickgsmith <patrickgromersmith@gmail.com>
2019-08-27 22:32:01 -0700

    moved /client into /server for heroku deploy

--- server/models/index.js
+++ server/models/index.js
@@ -1,27 +1,17 @@
 const mongoose = require("mongoose"),
-  User = require("./user");
+  User = require("./user"),
+  { DB_PASSWORD, DB_USER } = process.env;
 
-// mongoose.set("debug", true);
-
-// Connect to a local Mongo instance for testing purposes
-
-mongoose.connect("mongodb://localhost:27017/test", {
-  useNewUrlParser: true, // Use new url parser instead of default deprecated one
-  useCreateIndex: true, //ensure index is deprecated use createindex instead.
-  keepAlive: true
-});
+mongoose.connect(
+  `mongodb://${DB_USER}:${DB_PASSWORD}@ds245532.mlab.com:45532/chefsmenu`,
+  {
+    useNewUrlParser: true, // Use new url parser instead of default deprecated one
+    useCreateIndex: true, //ensure index is deprecated use createindex instead.
+    keepAlive: true
+  }
+);
 
 User.collection.createIndex({ location: "2dsphere" });
-
-// Construct URI and connect to the database
-// const { DB_USER, DB_PASSWORD, DB_HOSTNAME, DB_NAME } = process.env;
-// mongoose.connect(
-//   `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOSTNAME}:27017/${DB_NAME}`,
-//   {
-//     useNewUrlParser: true, // Use new url parser instead of default deprecated one
-//     keepAlive: true
-//   }
-// );
 
 // Export all schemas
 module.exports.User = User;
